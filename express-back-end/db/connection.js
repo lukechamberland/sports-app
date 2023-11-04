bba0f8484b1ee7048dcf49fb65728a6a9399262c
Luke Chamberland <luke.chamberland@shaw.ca>
2023-11-04 11:33:27 +0000

    reconfigure files

--- express-back-end/db/connection.js
+++ express-back-end/db/connection.js
@@ -2,7 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.database_url 
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT
 });
 
 pool.connect((err) => {
