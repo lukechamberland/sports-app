4bffa0792f9673262b34e3036e8d2402d9d0c827
Luke Chamberland <luke.chamberland@shaw.ca>
2023-11-04 11:19:32 +0000

    fix env file

--- express-back-end/db/connection.js
+++ express-back-end/db/connection.js
@@ -2,11 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT
+  connectionString: process.env.database_url 
 });
 
 pool.connect((err) => {
