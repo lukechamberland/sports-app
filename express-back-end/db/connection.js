6db856623b296ef9cf252328047d32938f364d1b
Luke Chamberland <luke.chamberland@shaw.ca>
2023-11-04 11:37:42 +0000

    wip

--- express-back-end/db/connection.js
+++ express-back-end/db/connection.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log("This is the test: ", process.env.DB_HOST);
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
