0c2cdd6c16a39b126eaf68a28d1429cd10f2f46d
Luke Chamberland <luke.chamberland@shaw.ca>
2023-11-04 11:12:41 +0000

    move connection

--- express-back-end/helpers.js
+++ express-back-end/helpers.js
@@ -1,4 +1,4 @@
-const { pool } = require('../db/connection.js');
+const { pool } = require('./db/connection.js');
 
 const addToUsers = function (id, email, password, firstname, lastname, username) {
   return new Promise((resolve, reject) => {
