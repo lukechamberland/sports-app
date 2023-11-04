a31d6efe4949dc62314377ea6d3b1d4706b4e5f0
Luke Chamberland <luke.chamberland@shaw.ca>
2023-11-04 11:51:52 +0000

    wip

--- express-back-end/knexfile.js
+++ express-back-end/knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
       password : process.env.DB_PASS,
       database : process.env.DB_NAME,
       port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL,
+      ssl      : true,
     },
     migrations: {
       directory: './db/migrations',
@@ -23,7 +23,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10,
